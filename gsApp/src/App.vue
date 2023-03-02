b1198c4c699ac86cc3b2d46633e5c0b98264fe30
John Doe <125718798+moemi-nishino@users.noreply.github.com>
2023-03-03 02:31:39 +0900

    テキストフォームから緯度経度を取得し、ボタンを押すとAPIからデータを取得

--- gsApp/src/App.vue
+++ gsApp/src/App.vue
@@ -1,16 +1,16 @@
 <script setup>
+import Form from "./components/Form.vue"
 import axios from "axios"
-const openWeatherMapKey = "1e6a0afb89116ed8a742cf296abf1187"
-const lat = 35.68202052460928
-const lon = 139.76721063068268
 
-const getCurrentWeatherData = () => {
+const openWeatherMapKey = "1e6a0afb89116ed8a742cf296abf1187"
+
+const getCurrentWeatherData = (obj) => {
   axios
-  .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${openWeatherMapKey}`)
+  .get(`https://api.openweathermap.org/data/2.5/weather?lat=${obj.latitude}&lon=${obj.longitude}&appid=${openWeatherMapKey}`)
   .then(res => console.log(res))
 }
 </script>
 
 <template>
-  <button @click="getCurrentWeatherData">get Current Weather Data test</button>
+  <Form @submit-form="getCurrentWeatherData"/>
 </template>
---
+++ gsApp/src/components/Form.vue
@@ -0,0 +1,21 @@
+<template>
+    <form @submit.prevent="submitForm">
+        <input type="text" v-model="obj.longitude" placeholder="緯度を入力してください">
+        <input type="text" v-model="obj.latitude" placeholder="経度を入力してください">
+        <button type="submit">get Current Weather Data</button>
+    </form>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+
+const obj = reactive({ longitude: "139.76721063068268", latitude: "35.68202052460928" });
+
+const submitForm = () => {
+    emits("submit-form", obj)
+}
+
+const emits = defineEmits([
+    "submit-form"
+])
+</script>
