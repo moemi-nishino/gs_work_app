da39358f00a02aabc69aecb07405887c7f144a9b
John Doe <125718798+moemi-nishino@users.noreply.github.com>
2023-03-02 20:26:58 +0900

    Current Weather Data APIから東京駅の緯度経度を指定してデータを取得

--- gsApp/src/App.vue
+++ gsApp/src/App.vue
@@ -1,13 +1,16 @@
 <script setup>
 import axios from "axios"
+const openWeatherMapKey = "1e6a0afb89116ed8a742cf296abf1187"
+const lat = 35.68202052460928
+const lon = 139.76721063068268
 
-const getWeather = () => {
+const getCurrentWeatherData = () => {
   axios
-  .get("http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=1e6a0afb89116ed8a742cf296abf1187")
+  .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${openWeatherMapKey}`)
   .then(res => console.log(res))
 }
 </script>
 
 <template>
-  <button @click="getWeather">getOpenWeatherAPItest</button>
+  <button @click="getCurrentWeatherData">get Current Weather Data test</button>
 </template>
