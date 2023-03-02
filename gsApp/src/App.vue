3f12dffa2097c1266632906eb555b67a3ae49930
John Doe <125718798+moemi-nishino@users.noreply.github.com>
2023-03-02 19:45:00 +0900

    OpenWeatherAPIの取得テスト

--- gsApp/src/App.vue
+++ gsApp/src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
+import axios from "axios"
 
+const getWeather = () => {
+  axios
+  .get("http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=1e6a0afb89116ed8a742cf296abf1187")
+  .then(res => console.log(res))
+}
 </script>
 
 <template>
-
+  <button @click="getWeather">getOpenWeatherAPItest</button>
 </template>
