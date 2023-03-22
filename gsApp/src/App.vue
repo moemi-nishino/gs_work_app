323c4a660d2353c7cb09b8823991be35ba744e5a
John Doe <125718798+moemi-nishino@users.noreply.github.com>
2023-03-22 21:31:32 +0900

    APIキーを環境変数から呼び出すように変更

--- gsApp/src/App.vue
+++ gsApp/src/App.vue
@@ -2,13 +2,14 @@
 import Form from "./components/Form.vue"
 import axios from "axios"
 
-const openWeatherMapKey = "1e6a0afb89116ed8a742cf296abf1187"
+const openWeatherMapKey = import.meta.env.VITE_WEATHERMAP_APIKEY
 
 const getCurrentWeatherData = (obj) => {
   axios
   .get(`https://api.openweathermap.org/data/2.5/weather?lat=${obj.latitude}&lon=${obj.longitude}&appid=${openWeatherMapKey}`)
   .then(res => console.log(res))
 }
+
 </script>
 
 <template>
